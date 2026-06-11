967d6b05a92eba794061eab5e375a2a5623a3349
Nadeesh-ld <naadeesh369@gmail.com>
2026-06-11 12:37:35 +0530

    day22

--- frontend/src/components/productDeleteModal.jsx
+++ frontend/src/components/productDeleteModal.jsx
@@ -41,7 +41,7 @@ export default function ProductDeleteModal(props){
             {
                 isModalOpen&&
                 <div className="w-screen h-screen fixed bg-black/30 top-0 left-0 flex justify-center items-center text-secondary">
-                    <div className="w-125 h-50 bg-white flex flex-col justify-center items-center rounded-lg p-4">
+                    <div className="w-[500px] h-[200px] bg-white flex flex-col justify-center items-center rounded-lg p-4">
                         <h1 className="text-xl font-bold mb-4">Are you sure you want to delete product with id {product.productId}?</h1>
                         <div className="flex gap-4">
                             <button className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700"
--- frontend/src/components/trendingProducts.jsx
+++ frontend/src/components/trendingProducts.jsx
@@ -1,13 +1,32 @@
-
 import ProductCard from "./productCard";
-export default function TrendingProducts(){
-    return (
-        <>
-    <div>
-        <h1>Trending Products</h1>
-              <ProductCard name="Laptop" price="15000lkr" image="https://tse3.mm.bing.net/th/id/OIP.ajWQ2LWuEe-RsP5Z_Q2X-AHaE8?rs=1&pid=ImgDetMain&o=7&rm=300" />
-               <ProductCard name="Phone" price="15000lkr" image="https://tse3.mm.bing.net/th/id/OIP.ajWQ2LWuEe-RsP5Z_Q2X-AHaE8?rs=1&pid=ImgDetMain&o=7&rm=300" />
-    </div>
-    </>
-    )
+
+export default function TrendingProducts() {
+	return (
+		<div>
+			<h1>Trending Products</h1>
+			<ProductCard
+				name="Mac book air"
+				price="150,000"
+				image="https://picsum.photos/id/1/200/300"
+			/>
+
+			<ProductCard
+				name="Dell XPS 13"
+				price="120,000"
+				image="https://picsum.photos/id/2/200/300"
+			/>
+
+			<ProductCard
+				name="HP Spectre x360"
+				price="180,000"
+				image="https://picsum.photos/id/3/200/300"
+			/>
+
+			<ProductCard
+				name="Lenovo ThinkPad X1"
+				price="165,000"
+				image="https://picsum.photos/id/4/200/300"
+			/>
+		</div>
+	);
 }
